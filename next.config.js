e3013ebd63dfcc3c0591e1d3bece078dcdc18a6b
Ertuğrul Altuntaş <ertgrlaltnts@gmail.com>
2024-10-04 13:06:33 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve = {
+        ...config.resolve,
+        preferRelative: true
+      };
+      return config;
+    }
+  };
